4adf92ca0ef369ca0cf81cd742f98a1635af691e
Noah <noah.moussaoui@student.uclouvain.be>
2026-04-11 13:14:08 +0200

    Clarification de la nomencalture de la table Users

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,4 +1,4 @@
-#fichier tests 
+# fichier tests
 from app import create_app
 from app.db import get_db
 from app.models import init_db
@@ -7,16 +7,16 @@ app = create_app()
 
 with app.app_context():
     db = get_db()
-    
-    #On crée les tables
+
+    # On crée les tables
     print("Construction des tables...")
     init_db(db)
-    
+
     # On insère un utilisateur de test
     try:
         db.execute(
-            "INSERT INTO users (username,Age, email, password) VALUES (?, ?, ?,?)",
-            ("TestUser", 34, "test@gaia.com", "password123")
+            "INSERT INTO Users (Username,Age, Email, Password) VALUES (?, ?, ?,?)",
+            ("TestUser", 34, "test@gaia.com", "password123"),
         )
         db.commit()
         print("✅ Utilisateur inséré avec succès !")
@@ -24,8 +24,10 @@ with app.app_context():
         print(f"❌ Erreur d'insertion (peut-être qu'il existe déjà ?) : {e}")
 
     # On vérifie si on peut le lire
-    user = db.execute("SELECT * FROM users WHERE username = ?", ("TestUser",)).fetchone()
-    
+    user = db.execute(
+        "SELECT * FROM users WHERE Username = ?", ("TestUser",)
+    ).fetchone()
+
     if user:
         print(f"🔍 Lecture réussie ! ID: {user['id']}, Pseudo: {user['username']}")
     else:
